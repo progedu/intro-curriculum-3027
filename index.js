f965979f91e5ba0eaa2ab6ad6f7e0cd626a1728f
Davideryu_Orihara <davideryu_orihara@dwango.co.jp>
2016-01-26 03:22:22 +0000

    OS コマンドインジェクション脆弱性が無いよう修正

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 let http = require('http');
-let cp = require('child_process');
 let server = http.createServer((req, res) => {
   let path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  res.end(path);
 });
 let port = 8000;
 server.listen(port, () => {
