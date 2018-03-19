0e21849b8d9099fe2bdbfaff37222eaf66dfbd31
nanae <desperado.kazune@gmail.com>
2018-03-19 11:15:09 +0000

    OSコマンドインジェンションに対する脆弱性を排除

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const http = require('http');
 const cp = require('child_process');
 const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain; charset=utf-8'
+  });
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  res.end(path);
 });
 const port = 8000;
 server.listen(port, () => {
