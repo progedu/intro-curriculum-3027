8998047066d1257c9ef21b919a8f02dbee81294f
nlnlnul <walking_5km@yahoo.co.jp>
2018-11-18 08:37:43 +0000

    execSync 関数を削除

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 const http = require('http');
-const cp = require('child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  res.end(path);
 });
-const port = 8000;
+const port = 8001;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
