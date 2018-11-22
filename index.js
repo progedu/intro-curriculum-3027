6ff91993912958425b7cdf8955b234a60688ba21
Ryoji Kimura <kmr1@me.com>
2018-11-22 11:06:11 +0000

    OS コマンドインジェクション脆弱性を修正

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
 const http = require('http');
-const cp = require('child_process');
+const moment = require('moment-timezone'); //日付変換モジュール
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  const now = moment(new Date()).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');//日付取得
+  console.info('アクセスされたパス ' + now + ' ' + path); //ログにも出力
+  res.writeHead(200, { //ヘッダ出力
+    'Content-Type': 'text/plain; charset=utf-8'
+  });
+  res.end(now + ' ' + path); //時間とパスを返す
 });
 const port = 8000;
 server.listen(port, () => {
