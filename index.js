729f81f2f0f57191be9d465f84de5a673a316ca8
yamatatu00 <tattuxan@swan.ocn.ne.jp>
2020-03-18 08:32:43 +0900

    OS コマンドインジェクション脆弱性のある HTTP サーバーを、この脆弱性がないように修正

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// OS コマンドインジェクション脆弱性のある HTTP サーバーを、この脆弱性がないように修正してみましょう。
 'use strict';
 const http = require('http');
-const cp = require('child_process');
+// const cp = require('child_process');//削除
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  // res.end(cp.execSync('echo ' + path));//削除
+  res.end(path);//追加
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
+
+// execSync のような OS コマンドを実行する関数はなるべく使わないことが望ましいです。
