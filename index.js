7de4c7d5f8d2b9eab39db143cf0c58beae1855ab
chip-cocoa2020 <arkbuchessener1209@outlook.jp>
2020-04-18 16:36:53 +0900

    OS injection 脆弱性を解消

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 const http = require('http');
-const cp = require('child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  res.writeHead(200, {
+    'Content-type': 'text/plain; charset=utf-8' 
+  });
+  res.write(`パス ${path} にアクセスしました。`);
+  console.log(`パス ${path} にアクセスしました。`);
+  res.end();
 });
 const port = 8000;
 server.listen(port, () => {
