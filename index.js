f14620852d03d29bed30317ae58d6c5e67c86277
miyatoriOIL <miyatorioil@gmail.com>
2020-06-30 15:09:29 +0900

    脆弱性の修正

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 const http = require('http');
-const cp = require('child_process');
+//const cp = require('child_process');
 const server = http.createServer((req, res) => {
-  const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+    const path = req.url;
+    res.end(path);
 });
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+    console.info('Listening on ' + port);
 });
