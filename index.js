a1c55fe0f66062e98fa2e5237b29d54d6032150f
shinfujita <shin4522@gmail.com>
2020-11-17 11:51:30 +0900

    3章28節の復習

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
 const http = require('http');
+const cp = require('child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(path);
+  res.writeHead(200, {
+    'Content-Type': 'text/plain; charset=utf-8'
+  });
+  res.end('このリクエストのパスは' + path + 'です。\r' + 'さらにコマンドラインでdateを入力：' + cp.execSync('date'));
+  // res.end(cp.execSync('echo ' + path));
 });
+
 const port = 8000;
 server.listen(port, () => {
   console.info(`Listen on ${port}`);
