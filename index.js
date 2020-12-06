ee8fcf3635460144d5a991f312436335d59266ed
vagrant <vagrant@vagrant.vm>
2020-12-06 10:08:03 +0900

    osインジェクション大作

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 const http = require('http');
-const cp = require('child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  res.writeHead('200', {
+    'Content-Type': 'text/html; charset=utf-8'
+  })
+  res.write(path)
+  res.end();
+  ;
 });
 const port = 8000;
 server.listen(port, () => {
