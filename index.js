458cd1efca74284436d242aae4169f7793523e47
hiroshi sato <40625222+heavygears@users.noreply.github.com>
2020-12-08 20:31:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const server = http.createServer((req, res) => {
   const path = req.url;
   res.end(path);
 });
+
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
