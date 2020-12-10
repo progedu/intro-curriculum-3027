26bceb2b7080866f3699017ce1bfa674bf69bb14
aoi <hokugin12@gmail.com>
2020-12-10 13:51:52 +0900

    execSyncを使わない

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const cp = require('child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  res.end(path);
 });
 const port = 8000;
 server.listen(port, () => {
