6c2640025992748f92876c08da6d7c4f161035a0
Yoshiaki <yyyadachi0@gmail.com>
2020-12-25 15:21:03 +0900

    パスを安全に表示

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const http = require('http');
 const cp = require('child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  res.writeHead(200, {
+    'Content-Type': 'text/plain, charhset=utf=8'
+  });
+  res.end(path);
 });
 const port = 8000;
 server.listen(port, () => {
