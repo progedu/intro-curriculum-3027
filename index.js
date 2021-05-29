372442c6bfbfa016bfb6482a1b9e17814cb4aef6
Kengo Yagishita <yagishitakengo1002@gmail.com>
2021-05-29 12:03:53 +0900

    OSコマンドインジェクション対策の実装

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 const http = require('http');
-const cp = require('child_process');
+//const cp = require('child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
-  res.end(cp.execSync('echo ' + path));
+  //res.end(cp.execSync('echo ' + path));
+  // OSコマンドインジェクション防止のため、execSync関数は使用しない
+  res.end(path);
 });
 const port = 8000;
 server.listen(port, () => {
